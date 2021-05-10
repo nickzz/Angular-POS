a0decb1b981ad1055495952bed402384351089d8
Hashif <nikhashif92@gmail.com>
2021-05-10 16:46:32 +0800

    create shared module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+  ]
+})
+export class MaterialModule { }
